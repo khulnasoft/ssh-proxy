78ac31d65233e582a23f2a0e3abbb7a726be1e00
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-07-10 02:35:21 +0600

    Initial commit

---
+++ docker-res/scripts/clean-layer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# This scripts should be called at the end of each RUN command
+# in the Dockerfiles.
+#
+# Each RUN command creates a new layer that is stored separately.
+# At the end of each command, we should ensure we clean up downloaded
+# archives and source files used to produce binary to reduce the size
+# of the layer.
+set -e
+set -x
+
+# Delete old downloaded archive files 
+apt-get autoremove -y
+# Delete downloaded archive files
+apt-get clean
+# Delete source files used for building binaries
+rm -rf /usr/local/src/*
+# Delete cache and temp folders
+rm -rf /tmp/* /var/tmp/* /root/.cache/* /var/cache/apt/*
+# Remove apt lists
+rm -rf /var/lib/apt/lists/* /etc/apt/sources.list.d/*
+
+# Clean npm
+if [ -x "$(command -v npm)" ]; then
+    npm cache clean --force
+    rm -rf /root/.npm/* /root/.node-gyp/*
+fi
